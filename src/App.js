0407e4263a222eb66cbea4b6ea5ba254f5413d7f
Petr Reshetin <preshetin@gmail.com>
2018-07-17 22:17:32 +0300

    Github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Header from './Header'
 import FilteredArtifactsBlock from './Artifacts/FilteredArtifactsBlock'
 import Spells from './Spells/Spells'
